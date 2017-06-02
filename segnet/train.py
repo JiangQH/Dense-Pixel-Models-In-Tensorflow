cb5bda35cbb1aa7414c1b832ad46d8391fb2ef56
jiangqinhong <qinhongjiang@zju.edu.cn>
2017-06-02 22:33:18 +0800

    change segnet train file

--- segnet/train.py
+++ segnet/train.py
@@ -6,31 +6,38 @@ parent_dir = os.getcwd()
 path = osp.dirname(parent_dir)
 sys.path.append(path)
 from model import Model
-from common.denseinput import DenseInput
-from common.loss import compute_dot_loss
+from common.dataset import BathLoader
+from common.loss import compute_dot_loss, compute_euclidean_loss
 from common.util import load_config
 import tensorflow as tf
 import time
 import numpy as np
 import argparse
+import simplejson
 
+MAX_ITER = 9999999
 def solve(config):
     with tf.Graph().as_default() as g:
-        # construct the data pipline
-        images, labels = DenseInput(config).densedata_pipelines()
+
+        images = tf.placeholder(dtype=tf.float32, shape=[config.batch_size,
+                                                         config.image_size[1], config.image_size[0], 3])
+        if config.label_channel == 1:
+            labels = tf.placeholder(dtype=tf.float32, shape=[config.batch_size, config.label_size[1],
+                                                             config.label_size[0]])
+        else:
+            labels = tf.placeholder(dtype=tf.float32, shape=[config.batch_size, config.label_size[1],
+                                                             config.label_size[0], 3])
+
+        is_training = tf.placeholder(dtype=tf.bool, name='is_training')
+        data_loader = BathLoader(config)
         # infere the output
-        predictions = Model(config).inference(images)
+        predictions = Model(config).inference(images, is_training)
         # the loss
-        #loss = compute_euclidean_loss(predictions, labels, masks)
-        loss = compute_dot_loss(predictions, labels)
+        loss = compute_euclidean_loss(predictions, labels)
+        #loss = compute_dot_loss(predictions, labels)
         # train op, use the sgd with momentum
         global_step = tf.Variable(0, name='global_step', trainable=False)
-        #learning_rate = tf.train.exponential_decay(learning_rate=config.lr,
-        #                                           global_step=global_step, decay_steps=config.stepsize,
-        #                                           decay_rate=config.gamma)
-        #train_op = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=config.momentum).minimize(
-        #    loss, global_step=global_step
-        #)
+
         train_op = tf.train.AdamOptimizer(learning_rate=config.lr).minimize(loss, global_step=global_step)
 
         # the saver to load params of pretrained model
@@ -47,39 +54,57 @@ def solve(config):
             else:
                 ckpt_path = ckpt.model_checkpoint_path
                 saver.restore(sess, ckpt_path)
-            #for var in tf.global_variables():
-            #    print var
-            # begin training
-            #for var in tf.trainable_variables():
-            #    print var
-            coord = tf.train.Coordinator()
-            threads = tf.train.start_queue_runners(sess, coord)
+            train_losses = []
+            val_losses = []
             print 'begin training now....'
             start_time = time.time()
             local_time = time.time()
-            for step in xrange(config.max_iter+1):
-                _, loss_val, pre, gt = sess.run([train_op, loss, predictions, labels])
-                if step % config.display == 0 or step == config.max_iter:
-                    print '{}[iterations], train loss {}, time consumes {}'.format(step,
-                                                                                  loss_val,
+            for step in xrange(MAX_ITER+1):
+                imgs, gts = data_loader.next_train_batch()
+                train_feed_dict = {images:imgs, labels:gts, is_training:True}
+                _, loss_train = sess.run([train_op, loss], feed_dict=train_feed_dict)
+                if step % config.display == 0 or step == MAX_ITER:
+                    print '{}[iterations], time consumes {}, train loss {}'.format(step,
+                                                                                  loss_train,
                                                                                   time.time()-local_time)
                     local_time = time.time()
-                #plt.figure(1)
-                #plt.imshow(np.uint8(rgb_masked[0, ...]))
-                #plt.figure(2)
-                #plt.imshow(np.uint8((normal_masked[0, ...]/2 + 0.5) * 255))
-                #plt.figure(3)
-                #plt.imshow(np.uint8(mask_num[0,...]))
-                assert not np.isnan(loss_val), 'model with loss nan'
+                assert not np.isnan(loss_train), 'model with loss nan'
+                train_losses.append(loss_train)
 
-                if step != 0 and (step % config.snapshot == 0 or step == config.max_iter):
-                    print 'saving snapshot...'
-                    saver.save(sess, osp.join(config.model_dir, 'model.ckpt'), global_step=step)
+                if step % config.test_iter == 0 or step == MAX_ITER:
+                    print '.............testing model..............'
+                    imgs, gts = data_loader.next_val_batch()
+                    val_feed_dict = {images: imgs, labels: gts, is_training: False}
+                    loss_val = sess.run([loss], feed_dict=val_feed_dict)
+                    val_losses.append(loss_val)
+                    print '{}[iterations], val loss {}'.format(step, loss_val)
 
-            coord.request_stop()
-            coord.join(threads)
-            print 'done, total consumes time {}'.format(time.time() - start_time)
+                if step != 0 and (step % config.snapshot == 0 or step == MAX_ITER):
+                    print '..............snapshot model.............'
+                    imgs, gts = data_loader.next_val_batch()
+                    val_feed_dict = {images: imgs, labels: gts, is_training: False}
+                    loss_val = sess.run([loss], feed_dict=val_feed_dict)
+                    val_losses.append(loss_val)
+                    print '{}[iterations], val loss {}'.format(step, loss_val)
+                    saver.save(sess, osp.join(config.model_dir, 'segnet_model.ckpt'), global_step=global_step)
 
+                # should we stop now ? can add accuracy support later
+                if data_loader.get_epoch() == config.max_epoch + 1:
+                    imgs, gts = data_loader.next_val_batch()
+                    val_feed_dict = {images: imgs, labels: gts, is_training: False}
+                    loss_val = sess.run([loss], feed_dict=val_feed_dict)
+                    val_losses.append(loss_val)
+                    print '{}[iterations], val loss {}'.format(step, loss_val)
+                    saver.save(sess, osp.join(config.model_dir, 'segnet_model.ckpt'), global_step=global_step)
+                    break
+
+            print 'total time comsums {}'.format(time.time() - start_time)
+            with open('train_loss_log.txt', 'w') as f:
+                simplejson.dump(train_losses, f)
+                f.close()
+            with open('val_loss_log.txt', 'w') as f:
+                simplejson.dump(val_losses, f)
+                f.close()
             sess.close()
 
 
